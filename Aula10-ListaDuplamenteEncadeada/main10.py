20ce69d2a43aa553d166678e196343b4d9643318
ADALTO SELAU SPARREMBERGER <assparremberger@fspoa.br>
2023-10-25 11:07:28 -0300

    Aula10 - 18/10/2023 - Lista Duplamente Encadeada

---
+++ Aula10-ListaDuplamenteEncadeada/main10.py
@@ -0,0 +1,18 @@
+from Lista import Lista
+
+dupla = Lista()
+
+dupla.imprimir()
+
+dupla.add( 10 )
+dupla.add( 5 )
+dupla.add( 20 )
+dupla.add( 15 )
+dupla.imprimirReverso()
+dupla.remover( 25 )
+dupla.remover( 5 )
+dupla.remover( 15 )
+print("Remover do Fim")
+dupla.remover( 20 )
+dupla.imprimirReverso()
+dupla.remover( 10 )
